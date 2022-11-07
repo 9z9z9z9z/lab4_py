3473d56aec2dd9c607cb5a66282d3aa2155644c9
9z9z9z9z <rolyan293@gmail.com>
2022-11-07 18:46:17 +0300

    end

---
+++ Equipment.py
@@ -0,0 +1,20 @@
+from Product import Product
+
+
+class Equipment(Product):
+    def __init__(self, name, price, type, weight, width=1, depth=1):
+        super(Equipment, self).__init__(name, price, weight, width, depth)
+        self.__type = type
+
+    @property
+    def type(self): return self.__type
+
+    @type.setter
+    def type(self, value):
+        self.__type = value
+
+
+
+    def __str__(self):
+        return "\n====================================\nName:\t" + self.name + "\nPrice:\t" + str(self.price) + "\nType:\t" + self.__type\
+               + "\nDimensions:\t" + str(self.volume) + "\n====================================\n"
--- lab_4_1_1.py
+++ lab_4_1_1.py
@@ -1,40 +1,11 @@
-from _distutils_hack import override
+from Equipment import Equipment
+from Food import Food
 
+base = [Equipment("Skies", 300, "Sport_Equipment", 300, 20, 5),
+        Equipment("Tanks", 3000, "War_Equipment", 30000, 2000, 500),
+        Food("Chicken", 120, (5, 11, 2022), 10, 15, 10, 5)]
 
-class Product:
-    def __init__(self, name, price, weight, width = 1, depth = 1):
-        self.__name = name
-        self.__price = price
-        self.__volume = (weight * width * depth)
-
-    def discount(self, percent):
-        ans = round((self.price * (100 - percent) / 100), 2)
-        if ans == 0:
-            return 0.01
-        else:
-            return ans
-
-    def count(self, weight, width, depth): return self.volume // (weight * width * depth)
-
-    @property
-    def price(self): return self.__price
-
-    @property
-    def volume(self): return self.__volume
-
-    @price.setter
-    def price(self, price): self.__price = price
-
-    @volume.setter
-    def volume(self, volume): self.__volume = volume
-
-    def __add__(self, other): return Product(self.__name + "\n" + other.__name, self.__price + other.__price, self.__volume + other.__volume)
-
-    def __str__(self):
-        return "Name: \t" + self.__name + "\nPrice:\t" + str(self.__price) + "\nVolume:\t" + str(self.__volume)
-
-
-product1 = Product("Mef", 100, 200, 1, 1)
-product2 = Product("Gay", 200, 100, 1, 1)
-print(product1, "\n\n")
-print(product1 + product2)
+print((base[0] + base[2]).count(1000000, 1, 1))
+print(base[0] + base[2])
+print(base[0])
+print(base[2])
